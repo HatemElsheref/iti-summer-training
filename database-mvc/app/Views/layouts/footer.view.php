8dd7c803b86d2557ea36195f82e8283f9a3cee8b
Hatem Mohamed Elsheref <hatemelsheref99@gmail.com>
2020-09-07 13:48:38 +0200

    remove un needed files from the main project

--- database-mvc/app/Views/layouts/footer.view.php
+++
@@ -1,9 +0,0 @@
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
-<script src="<?=assets('js/jquery-3.5.1.slim.min.js')?>" crossorigin="anonymous"></script>
-<script src="<?=assets('js/popper.min.js')?>" crossorigin="anonymous"></script>
-<script src="<?=assets('js/bootstrap.min.js')?>" crossorigin="anonymous"></script>
-</body>
-</html>
